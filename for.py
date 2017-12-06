14f20a883c663939332dc2bf26eac0082e6c61ce
mspzyzh <mspzyzh@gmail.com>
2017-12-06 13:09:46 +0800

    update

---
+++ for.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+names = ('Michael','Bob','Tracy')   #元组 tuple = () tuple一旦初始化就不能修改
+for name in names:
+    print name
+
+i = [1,2,3,4,5,6,7,8,9,10]  #列表 list = [] list是一种有序的集合，可以随时添加和删除其中的元素
+sum = 0
+for x in i:
+    sum = sum + x
+print sum
+
+sum = 0
+for x in range(101):    #range()函数，可以生成一个整数序列，比如range(5)生成的序列是从0开始小于5的整数
+    sum = sum + x
+print sum
---
+++ while.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+sum = 0
+n = 99
+while n > 0:    #while循环，只要条件满足，就不断循环，条件不满足时退出循环
+    sum = sum + n
+    n = n - 2
+print sum
