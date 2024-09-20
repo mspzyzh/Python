f7497f82bc2fb81dde6215d8a559789be643f54f
mspzyzh <mspzyzh@qq.com>
2024-09-20 11:23:42 +0800

    运算符

---
+++ 运算符.py
@@ -0,0 +1,12 @@
+"""
+python 运算符示例
+"""
+a = 13
+b = 5
+print("13 + 5=",a + b)
+print("13 - 5=",a - b)
+print("13 * 5=",a * b)
+print("13 ÷ 5=",a / b)
+print("13 // 5=",a // b)
+print("13 % 5=",a % b)
+print("13 ** 5=",a ** b)
